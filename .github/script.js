9385ec53d5dc9b6890e0ae167105283b6bf4af91
Emilly3008 <151835514+Emilly3008@users.noreply.github.com>
2024-01-03 12:19:24 +0100

    paginaweb1

---
+++ .github/script.js
@@ -0,0 +1,11 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    /// Tu código JavaScript aquí...
+    /// Puedes cargar datos JSON y manipularlos en esta sección...
+
+
+
+
+
+
+});
